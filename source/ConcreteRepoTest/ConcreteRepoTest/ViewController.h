8b58a61518c4965fa7f6630edc6a668b157bacd6
Viktor <viktor>
2014-05-16 02:14:07 +0200

    base iOS Xcode project and ignores

---
+++ source/ConcreteRepoTest/ConcreteRepoTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ConcreteRepoTest
+//
+//  Created by Viktor Benei on 5/16/14.
+//  Copyright (c) 2014 Concrete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ source/ConcreteRepoTest/ConcreteRepoTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ConcreteRepoTest
+//
+//  Created by Viktor Benei on 5/16/14.
+//  Copyright (c) 2014 Concrete. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
